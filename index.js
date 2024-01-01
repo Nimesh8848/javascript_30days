ad126ca807f3ecc04e9a9c65a16bd1d7d46bc7f7
Nimesh8848 <nimeshmathema@gmail.com>
2024-01-01 17:09:37 +0545

    second day

--- index.js
+++ index.js
@@ -37,6 +37,46 @@ const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
 // }
 // console.log(person.getFullName());
 
+// const person = {
+//     firstName: 'Asabeneh',
+//     lastName: 'Yetayeh',
+//     age: 250,
+//     country: 'Finland',
+//     city: 'Helsinki',
+//     skills: [
+//       'HTML',
+//       'CSS',
+//       'JavaScript',
+//       'React',
+//       'Node',
+//       'MongoDB',
+//       'Python',
+//       'D3.js',
+//     ],
+//     getFullName: function () {
+//       return `${this.firstName} ${this.lastName}`
+//     },
+//   }
+//   person.nationality = 'Ethiopian'
+//   person.country = 'Finland'
+//   person.title = 'teacher'
+//   person.skills.push('Meteor')
+//   person.skills.push('SasS')
+//   person.isMarried = true
+  
+//   person.getPersonInfo = function () {
+//     let skillsWithoutLastSkill = this.skills
+//       .slice(0, this.skills.length - 1)
+//       .join(', ')
+//     let lastSkill = this.skills.slice(this.skills.length - 1)[0]
+  
+//     let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
+//     let fullName = this.getFullName()
+//     let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`
+//     return statement
+//   }
+//   console.log(person)
+//   console.log(person.getPersonInfo())
 const person = {
     firstName: 'Asabeneh',
     lastName: 'Yetayeh',
@@ -77,3 +117,10 @@ const person = {
   }
   console.log(person)
   console.log(person.getPersonInfo())
+
+  function square(){
+    let num=2;
+    sq=num*num;
+    console.log(sq);
+  }
+  square();
